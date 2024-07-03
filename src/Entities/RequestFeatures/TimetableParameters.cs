8d412cdd683715c0edfce836aa6c8711d601b1ee
Lam Phan <ponpy30@gmail.com>
2024-07-03 13:44:46 +0700

    [update] - update timetable param

--- src/Entities/RequestFeatures/TimetableParameters.cs
+++ src/Entities/RequestFeatures/TimetableParameters.cs
@@ -10,11 +10,17 @@ namespace Entities.RequestFeatures
         public List<TimetableUnitTCDTO> FreeTimetableUnits { get; set; } = []; // Ds tiết trống - dùng cho kiểm tra tiết lủng // bỏ
         public List<TimetableUnitTCDTO> NoAssignTimetableUnits { get; set; } = []; // Ds tiết không xếp
         public List<TimetableUnitTCDTO> BusyTimetableUnits { get; set; } = []; // Ds tiết bận - dùng cho kiểm tra lịch bận của gv
-        public Dictionary<Guid, int> SubjectsWithPracticeRoom { get; set; } = [];
+        public List<SubjectWithPracticeRoom> SubjectsWithPracticeRoom { get; set; } = [];
         public int MaxPeriodPerDay { get; set; } = 5;
         public int MinPeriodPerDay { get; set; } = 0;
         public int StartYear { get; set; }
         public int EndYear { get; set; }
         public int Semester { get; set; }
+    }
+
+    public class SubjectWithPracticeRoom
+    {
+        public Guid SubjectId { get; set; }
+        public int RoomCount { get; set; } = 0;
     }
 }
